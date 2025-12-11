82e4f4dbcabe263a360b01a3de5d7fffa2d64738
akanto99 <mdnirobkhan.akanto99@gmail.com>
2025-12-11 12:25:42 +0600

    Play Store deployment Issue Fix

--- lib/configs/res/app_url.dart
+++ lib/configs/res/app_url.dart
@@ -1,7 +1,7 @@
 class AppUrl {
 
-  static var baseUrl = 'https://api-staging.dinmajur.com/api/v1' ;// Staging Server
-  // static var baseUrl = 'https://api.dinmajur.com/api/v1' ;// Dev
+  // static var baseUrl = 'https://api-staging.dinmajur.com/api/v1' ;// Staging Server
+  static var baseUrl = 'https://api.dinmajur.com/api/v1' ;// Dev
 
   static const String refreshTokenEndpoint = '/auth/refresh-token';
   static var loginEndPint = baseUrl + '/auth/login';
