ec71be5f38c776c8f111cf3bf617fa70fa272e8f
akanto99 <mdnirobkhan.akanto99@gmail.com>
2026-01-27 12:19:28 +0600

    Ongoing

--- lib/configs/res/components/header_appbar.dart
+++ lib/configs/res/components/header_appbar.dart
@@ -1,13 +1,59 @@
-import 'package:auto_size_text/auto_size_text.dart';
+// import 'package:auto_size_text/auto_size_text.dart';
+// import 'package:dinmajur_customer/configs/res/color.dart';
+// import 'package:dinmajur_customer/configs/res/text_styles.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
+//
+//
+// class AppBarHeader extends StatelessWidget {
+//   final String appTitle;
+//
+//   const AppBarHeader(this.appTitle, {super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final screenWidth = MediaQuery.of(context).size.width;
+//     final screenHeight = MediaQuery.of(context).size.height;
+//
+//     return Container(
+//       // padding: EdgeInsets.symmetric(horizontal: screenHeight * 0.02),
+//       height: 60,
+//       decoration: BoxDecoration(
+//         borderRadius: BorderRadius.only(
+//           bottomLeft: Radius.circular(24),
+//           bottomRight: Radius.circular(24),
+//         ),
+//         color: AppColors.containerBackground(context),
+//         border: Border(bottom: BorderSide(color: AppColors.border(context), width: 1.0)),
+//       ),
+//       child: Center(
+//         child: Container(
+//           width: screenWidth * 0.9,
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               Container(height: 20, width: 24,
+//                   color: Colors.transparent,
+//                   alignment: Alignment.centerLeft,
+//                   child: SvgPicture.asset("assets/images/header_arrow.svg")),
+//
+//               Text(appTitle, style: AppTextStyles.textSize24(context, weight: FontWeight.w600,color: AppColors.textPrimary(context))),
+//               Container(width: 20, height: 24),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:dinmajur_customer/configs/res/color.dart';
 import 'package:dinmajur_customer/configs/res/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
+import 'package:auto_size_text/auto_size_text.dart';
 
 class AppBarHeader extends StatelessWidget {
   final String appTitle;
-
   const AppBarHeader(this.appTitle, {super.key});
 
   @override
@@ -16,7 +62,6 @@ class AppBarHeader extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Container(
-      // padding: EdgeInsets.symmetric(horizontal: screenHeight * 0.02),
       height: 60,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.only(
@@ -24,7 +69,12 @@ class AppBarHeader extends StatelessWidget {
           bottomRight: Radius.circular(24),
         ),
         color: AppColors.containerBackground(context),
-        border: Border(bottom: BorderSide(color: AppColors.border(context), width: 1.0)),
+        border: Border(
+          bottom: BorderSide(
+            color: AppColors.border(context),
+            width: 1.0,
+          ),
+        ),
       ),
       child: Center(
         child: Container(
@@ -32,18 +82,25 @@ class AppBarHeader extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(height: 20, width: 24,
-                  color: Colors.transparent,
-                  alignment: Alignment.centerLeft,
-                  child: SvgPicture.asset("assets/images/header_arrow.svg")),
-
-              // Text(appTitle, style: AppTextStyles.textSize24(context, weight: FontWeight.w600,color: AppColors.textPrimary(context))),
+              // Left arrow (fixed width)
               Container(
-                height: 40,
-                child: Flexible(
+                height: 20,
+                width: 24,
+                alignment: Alignment.centerLeft,
+                child: SvgPicture.asset("assets/images/header_arrow.svg"),
+              ),
+
+              // Title (flexible - takes remaining space)
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
                   child: AutoSizeText(
                     appTitle,
-                    style: AppTextStyles.textSize24(context, weight: FontWeight.w600, color: AppColors.textPrimary(context)),
+                    style: AppTextStyles.textSize24(
+                      context,
+                      weight: FontWeight.w600,
+                      color: AppColors.textPrimary(context),
+                    ),
                     maxLines: 1,
                     minFontSize: 14,
                     maxFontSize: 24,
@@ -52,7 +109,9 @@ class AppBarHeader extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(width: 20, height: 24),
+
+              // Right spacer (fixed width to balance the left arrow)
+              Container(width: 24, height: 20),
             ],
           ),
         ),
