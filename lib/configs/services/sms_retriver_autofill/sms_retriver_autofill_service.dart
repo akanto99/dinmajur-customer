2c1fabf0f868a84885e7b90ca0bb04e240fda2a1
akanto99 <mdnirobkhan.akanto99@gmail.com>
2026-02-05 10:47:11 +0600

    OnGoing

---
+++ lib/configs/services/sms_retriver_autofill/sms_retriver_autofill_service.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:pinput/pinput.dart';
+import 'package:smart_auth/smart_auth.dart';
+
+class SmsRetrieverImpl implements SmsRetriever {
+  SmsRetrieverImpl(this.smartAuth, {this.onSmsReceived}) {
+    _startListening();
+  }
+
+  final SmartAuth smartAuth;
+  final Function(String?)? onSmsReceived;
+
+  // ✅ Start listening immediately when initialized
+  void _startListening() {
+    getSmsCode();
+  }
+
+  @override
+  Future<void> dispose() {
+    return smartAuth.removeUserConsentApiListener();
+  }
+
+  @override
+  Future<String?> getSmsCode() async {
+    try {
+      final res = await smartAuth.getSmsWithUserConsentApi();
+      if (res.hasData) {
+        debugPrint('SMS received: ${res.data?.sms}');
+        final code = res.data?.code;
+        if (code != null) {
+          debugPrint('OTP code extracted: $code');
+
+          // ✅ Trigger callback to update UI
+          onSmsReceived?.call(code);
+
+          return code;
+        }
+      }
+    } catch (e) {
+      debugPrint('Error getting SMS code: $e');
+    }
+    return null;
+  }
+
+  @override
+  bool get listenForMultipleSms => false;
+}
