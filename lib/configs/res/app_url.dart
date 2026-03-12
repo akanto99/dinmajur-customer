57f9f592f0bc8ca5f26e9089f928bbeee25048e6
akanto99 <mdnirobkhan.akanto99@gmail.com>
2026-03-12 18:34:28 +0600

    OnGoing

--- lib/configs/res/app_url.dart
+++ lib/configs/res/app_url.dart
@@ -1,6 +1,6 @@
 class AppUrl {
-  static var baseUrl = 'https://274d-103-153-170-227.ngrok-free.app/api/v1' ;// Staging Server
-  static var socketUrl = 'https://274d-103-153-170-227.ngrok-free.app' ;// Staging
+  static var baseUrl = 'https://05e3-103-153-170-227.ngrok-free.app/api/v1' ;// Staging Server
+  static var socketUrl = 'https://05e3-103-153-170-227.ngrok-free.app' ;// Staging
 
   // static var baseUrl = 'https://api-staging.dinmajur.com/api/v1' ;// Staging Server
   // static var socketUrl = 'https://api-staging.dinmajur.com' ;// Staging
